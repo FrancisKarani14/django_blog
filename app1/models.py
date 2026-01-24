0bc0659dfdb66e6713e0ef3139faccff4a3a182e
Francis <franciskarani15@gmail.com>
2026-01-24 11:22:19 +0300

    creates a User model using abstract user

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# user model
+class User(AbstractUser):
+    USER_TYPES = (
+        ('admin', 'Admin'),
+        ('user', 'User'),
+        
+    ) 
+    user_type = models.CharField(max_length=20, choices=USER_TYPES, default='user')
+    def __str__(self):
+        return self.username
