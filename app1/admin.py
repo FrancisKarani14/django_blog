f176beda479c17ac44b6bc52121baba723cac2b7
Francis <franciskarani15@gmail.com>
2026-01-24 12:25:44 +0300

    registers user model using UserAdmin class

--- app1/admin.py
+++ app1/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Post, User, Profile, Comment
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(Post, User, Comment, Profile)
+admin.site.register([Post, Comment, Profile])
+
+@admin.register(User)
+class CustomuserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'user_type', 'is_staff']
+    list_filter = ['user_type', 'is_staff', 'is_superuser', 'is_active']
+
 
