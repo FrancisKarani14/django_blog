21ba1d989fb4014606e8b123228f4dc613047a48
Francis <franciskarani15@gmail.com>
2026-01-24 14:50:09 +0300

    adds fieldsets in the user table registration

--- app1/admin.py
+++ app1/admin.py
@@ -1,14 +1,33 @@
 from django.contrib import admin
-from .models import Post, User, Profile, Comment
 from django.contrib.auth.admin import UserAdmin
+from .models import Post, User, Profile, Comment
 
-# Register your models here.
 
 admin.site.register([Post, Comment, Profile])
 
+
 @admin.register(User)
-class CustomuserAdmin(UserAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name', 'user_type', 'is_staff']
-    list_filter = ['user_type', 'is_staff', 'is_superuser', 'is_active']
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'user_type',
+        'is_staff',
+    )
 
+    list_filter = (
+        'user_type',
+        'is_staff',
+        'is_superuser',
+        'is_active',
+    )
 
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('user_type',)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('user_type',)}),
+    )
