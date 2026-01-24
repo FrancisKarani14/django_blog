59e13282bfb47051c65d452036786695776005f4
Francis <franciskarani15@gmail.com>
2026-01-24 21:27:38 +0300

    configures urls

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 # register urls
 urlpatterns = [
-    
+    path('', views.home, name='app1-home'),
 ]
 
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post, Comment
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'app1/home.html')
+
+# create a post
+def create_post(request):
+    pass
