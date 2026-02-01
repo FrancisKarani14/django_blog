40e72c6a3ada7555fb04f4ccc26507c6d7970f19
Francis <franciskarani15@gmail.com>
2026-02-01 16:53:36 +0300

    gets rid of all the views

--- app1/views.py
+++ app1/views.py
@@ -5,25 +5,3 @@ from models import Post, Comment
 from django.url import reverse_lazy
 
 # Create your views here.
-def home(request):
-    return render(request, 'app1/home.html')
-
-# create a post
-class create_post(CreateView):
-    model = Post
-    fields = ['title', 'content', 'author']
-    template_name = 'app1/create_post.html'
-    success_url =reverse_lazy('/') 
-
-# update post
-class update_post(UpdateView):
-    model = Post
-    fields = ['title', 'content']
-    template_name = 'app1/update_post.html'
-    success_url =reverse_lazy('/')
-
-# view all posts
-class view_posts(ListView):
-    model = Post
-    template_name = 'app1/view_posts.html'
-    context_object_name = 'posts'
