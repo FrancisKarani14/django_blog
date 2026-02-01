c20236d0f605a00050827cbcdc5ddb2c8bc45354
Francis <franciskarani15@gmail.com>
2026-02-01 21:40:58 +0300

    creates a CBV for making a post

--- app1/views.py
+++ app1/views.py
@@ -5,3 +5,9 @@ from models import Post, Comment
 from django.url import reverse_lazy
 
 # Create your views here.
+class CreatePostView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'createpost.html'
+    success_url = reverse_lazy('post_list')
+
